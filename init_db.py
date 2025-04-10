0e975724fc61349220449ec8f47087594756e872
PeytonHecht <62864282+PeytonHecht@users.noreply.github.com>
2025-04-10 13:51:29 -0400

    page numbers
    added visuals and page numbers to bottom of events page

--- init_db.py
+++ init_db.py
@@ -3,8 +3,8 @@ import psycopg2
 conn = psycopg2.connect(    
     host="localhost",
     database="stayfit_db",
-    user='',
-    password='', port='5432')
+    user='postgres',
+    password='Google232.', port='5432')
 # add own user and password
 cur = conn.cursor()
 
